4a62d2426d5ebeede6cb211cb73c7eec031ce457
Yinkaadx <yinkaadx@gmail.com>
2025-07-15 23:55:00 +0400

    remove port from config.toml to fix toml error

--- setup.sh
+++ setup.sh
@@ -2,4 +2,3 @@ mkdir -p ~/.streamlit/
 echo "[server]" > ~/.streamlit/config.toml
 echo "headless = true" >> ~/.streamlit/config.toml
 echo "enableCORS = false" >> ~/.streamlit/config.toml
-echo "port = \$PORT" >> ~/.streamlit/config.toml
